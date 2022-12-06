d88fefcf238cf26afef6738ba36889785024b456
esettes <ioanaroxana.st@gmail.com>
2022-12-06 23:07:43 +0000

    start to cumpute a fractal

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 /**
  * @ Author: Roxana Stancu (esettes)
  * @ Created: 2022/12/02 23:40
- * @ Modified: 2022/12/06 22:25
+ * @ Modified: 2022/12/06 23:02
  */
 
 #include "compute.h"
@@ -14,7 +14,32 @@
 /* Need to map GPUs memory to our memory */
 /* Buffers mirrors */
 uint32_t	g_in_data[1000];
-float		g_out_data[1000];
+uint32_t	g_out_data[1000];
+
+void	generate_fractal(void)
+{
+	uint32_t	height_size = 1000;
+	uint32_t	width_size = 1000;
+
+	for (uint32_t row = 0; row < height_size; row++)
+	{
+		for (uint32_t col = 0; col < width_size; col++)
+		{
+			float r = (float)row / 500.0 - 1.0;
+			float i = (float)col / 500.0 - 1.0;
+
+			uint32_t count = 0;
+			while ((r * r * i < 4.0) && (count < 1000))
+			{
+				float temp = r * r - i * i + 0.17;
+				i = 2.0 * r * i + i;
+				r = temp + r;
+				count++;
+			}
+			/** Color depends on the counter*/
+		}
+	}
+}
 
 int	main(int argc, char **argv)
 {
